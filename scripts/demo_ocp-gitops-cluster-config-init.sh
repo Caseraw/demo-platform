f4fce99e1fe21e27e73b957718d6adcd5f5b4315
Kasra <kasra.amirsarvari@gmail.com>
2024-02-23 03:54:21 +0000

    updates

--- scripts/demo_ocp-gitops-cluster-config-init.sh
+++ scripts/demo_ocp-gitops-cluster-config-init.sh
@@ -6,8 +6,11 @@ source demo_prep.sh
 # OpenShift GitOps
 source setup_openshift-gitops.sh
 
-# OpenShift DevSpaces
-source setup_openshift-devspaces.sh
+# # Platform ArgoCD
+# source setup_platform-argocd.sh
 
-# OpenShift Cert Manager
-source setup_openshift-cert-manager.sh
+# # OpenShift DevSpaces
+# source setup_openshift-devspaces.sh
+
+# # OpenShift Cert Manager
+# source setup_openshift-cert-manager.sh
